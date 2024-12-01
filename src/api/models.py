9479ee9ecc73710a9228adcc4a708a80bf2446d8
Martin-cedres <martinfernandocedres@gmail.com>
2024-12-01 19:12:43 +0000

    SERIALICE

--- src/api/models.py
+++ src/api/models.py
@@ -19,10 +19,36 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.user_name}>'
 
+def serialize(self):
+    return {
+        "id": self.id,
+        "username": self.user_name,
+        "email": self.email,
+        "role": self.role,
+        "status": self.status,
+        "created_at": self.created_at.isoformat() if self.created_at else None,
+        "updated_at": self.updated_at.isoformat() if self.updated_at else None
+    }
+
+class Reservations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.Foreingkey('user.id', nullable=False)) 
+    sart_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(pytz.utc))
+    updated_at = db.Column(db.DateTime, default=datetime.now(putz.utc), onupdate=datetime.now(pytz.utc))
+
+    def __repr__(self):
+        return f'<Reservations {self.event_name}>'
+    
     def serialize(self):
         return {
-            "id": self.id,
-            "username": self.user_name,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "id": self.id, 
+            "event_name": self.event_name,
+            "user_id": self.user_id, 
+            "sart_time": self.sart_time, 
+            "end_time": self.end_time, 
+            "created_at": self.created_at.isoformat() if self.created_at else None, 
+            "updated_at": self.updated_at.isoformar() if self.created_at else None, 
         }
