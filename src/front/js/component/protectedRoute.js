4cc9609b2ef3532b51ad780756a8a6ad33849df3
Martin-cedres <martinfernandocedres@gmail.com>
2024-12-13 12:36:30 +0000

    Agregue protectedRoute

---
+++ src/front/js/component/protectedRoute.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useContextApp } from '../store/appContext';
+
+
+// Componente que protege las rutas que solo deben ser accesibles por administradores
+const ProtectedRoute = ({ children, requiredRole }) => {
+    const { store } = useContextApp();  // Obtener el rol del usuario desde el contexto
+    const navigate = useNavigate();
+    const { location } = useLocation();  // Obtener la ruta actual
+
+    useEffect(() => {
+        // Si el rol del usuario no coincide con el requerido, redirige a otra página
+        if (store.user.role !== requiredRole) {
+            navigate("/");  // Redirigir al home
+        }
+    }, [store.user.role, location, navigate]);
+
+    return children;  // Si el usuario tiene el rol correcto, renderiza el contenido protegido
+};
+
+
+export { ProtectedRoute };
