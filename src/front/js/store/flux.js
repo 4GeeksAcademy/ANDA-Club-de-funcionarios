87a234caebd373f1b43cb41040f920360c70a7ab
Esteban Brea <83199483+estebanbrea@users.noreply.github.com>
2024-12-15 19:53:07 +0000

    [Solucionado problemas con editarcargarlibro]

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,6 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
     return {
         store: {
             message: null,
+            libros: [],
             demo: [
                 {
                     title: "FIRST",
