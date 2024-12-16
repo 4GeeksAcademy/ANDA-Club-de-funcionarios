7a205506b0ee672936586b18d2a0905ad8c9ebd0
Martin-cedres <martinfernandocedres@gmail.com>
2024-12-16 03:15:22 +0000

    Vistas Home y About

--- src/front/js/pages/About.js
+++ src/front/js/pages/About.js
@@ -10,6 +10,7 @@ const About = () => {
         </a>
       </div>
 
+
       <nav className="navbar" style={{ backgroundColor: '#eef2ff', height: '3rem' }}>
         <div className="container">
           <a className="navbar-brand" href="#"></a>
@@ -19,27 +20,19 @@ const About = () => {
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6 d-flex justify-content-end">
-            <img src={Interrogacion} className="d-block mx-lg-auto img-fluid"
-              alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+            <img src={Interrogacion} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
           </div>
           <div className="col-lg-6">
-            <h6 className="display-1 fw-bold lh-1 mb-3"
-              style={{ color: '#3865e5', fontSize: '4rem' }}>¿Qué es ANDA <i>Plus</i>?</h6>
-
-            <p><strong>ANDA <i>Plus</i></strong> es una plataforma diseñada para sus funcionario/as
-              que facilita la reserva en línea de un salón para eventos así como también el préstamo de libros.
-              Aquí podrás organizar actividades de manera fácil y eficiente, además de contar con otros beneficios exclusivos.</p>
-
+            <h6 className="display-1 fw-bold  lh-1 mb-3" style={{ color: '#3865e5', fontSize: '4rem' }}>¿Qué es el Club de Funcionarios?</h6>
+            <p>El Club de Funcionarios de ANDA es una plataforma diseñada
+              para facilitar la gestión de reservas de locales para eventos y préstamos de libros. Aquí podrás organizar tus
+              actividades de manera fácil y eficiente, además de disfrutar de múltiples beneficios exclusivos.</p>
             <h5 style={{ color: '#3865e5', fontSize: '1.5rem', marginBottom: '1rem' }}>¿Cómo usar nuestra plataforma?</h5>
-
             <strong> Reserva un local:</strong>
-
             <p> Ve a la sección de eventos, selecciona una fecha disponible y agrega una descripción del tipo de evento.</p>
-
             <strong>Préstamo de libros: </strong>
-
             <p>Accede a la biblioteca, elige un libro y realiza tu reserva.
-              Consultá tu historial para ver todas tus reservas y préstamos en un sólo lugar.</p>
+              Consulta tu historial para ver todas tus reservas y préstamos en un sólo lugar.</p>
 
             <h5 className="mt-5 mb-4">Preguntas Frecuentes</h5>
 
@@ -47,36 +40,38 @@ const About = () => {
               <div className="accordion-item">
                 <h2 className="accordion-header">
                   <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne" style={{ borderColor: "#3865e5" }}>
-                    ¿Como reservo un evento?
+                    <strong>¿Como reservo un salón para eventos?</strong>
                   </button>
                 </h2>
                 <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" style={{ borderColor: "#3865e5" }}>
                   <div className="accordion-body">
-                    Respuesta 1
+                    Dirígete a la sección Eventos en la barra superior de tu perfil y consulta la disponibilidad del salón en el calendario.
+                    Si no está disponible, serás notificado ni bien se encuentre disponible o te facilitaremos otras fechas disponibles vía mail o whapp.
                   </div>
                 </div>
               </div>
               <div className="accordion-item">
                 <h2 className="accordion-header">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                    ¿Que pasa si no devuelvo un libro a tiempo?
+                    <strong>¿Que pasa si no devuelvo un libro a tiempo?</strong>
                   </button>
                 </h2>
                 <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
                   <div className="accordion-body">
-                    Respuesta 2
+                    El préstamo de libros es de un mes a partir de la fecha que fue retirado. Serás notificado vía mail sobre el período de tu préstamo.
+                    Una vez vencido el plazo para la devolución, podrás comunicarte vía mail o WhatsApp avisando la demora.
                   </div>
                 </div>
               </div>
               <div className="accordion-item">
                 <h2 className="accordion-header">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                    ¿Como puedo cancelar una reserva?
+                    <strong>¿Como puedo cancelar una reserva?</strong>
                   </button>
                 </h2>
                 <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
                   <div className="accordion-body">
-                    Respuesta 3
+                    Podrás cancelar tus reservas en cualquier momento desde el Panel de Usuario, en la sección Mis Reservas.
                   </div>
                 </div>
               </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -20,6 +20,7 @@ export const Home = () => {
 					<a className="navbar-brand" href="#"></a>
 				</div>
 			</nav>
+			
 
 			<div className="container col-xxl-8 px-4 py-5">
 				<div className="row flex-lg-row-reverse align-items-center g-5 py-5">
@@ -27,8 +28,8 @@ export const Home = () => {
 						<img src={manos} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
 					</div>
 					<div className="col-lg-6">
-						<h3 className="display-1 fw-bold  lh-1 mb-3" style={{ color: '#3865e5' }}>ANDA<i>Plus</i> </h3>
-						<p className="lead fw-bold" style={{ fontSize: '1.5rem' }} >¡Disfruta de los beneficios pensados para vos!</p>
+						<h1 className="display-1 fw-bold  lh-1 mb-3" style={{ color: '#3865e5' }}>Club de funcionarios</h1>
+						<p className="lead fw-bold" style={{ fontSize: '1.5rem' }} >¡Disfruta de los beneficios pensados para ti!</p>
 						<div className="d-grid gap-2 d-md-flex justify-content-md-start" style={{ marginTop: '5rem' }}>
 							<Link to="/login" >
 								<button type="button" className=" btn btn-lg px-4 me-md-2"
